d6d84e5a6317df02e5f786b5980a59fb1f51acb7
Jorge201105 <jimunozacuna@gmail.com>
2025-06-24 02:25:00 +0000

    W

---
+++ error.py
@@ -0,0 +1,17 @@
+
+
+class DimensionError(Exception):
+    def __init__(self, mensaje, dimension , maximo):
+        self.mensaje = mensaje
+        self.dimension = dimension
+        self.maximo = maximo
+
+    def __str__(self):
+        # si no me pasamos la dimension y no le pasanmos el maximo retornar el str del padre
+        if self.dimension is None and self.maximo is None:
+            return super().__str__()
+        else:
+            return f"{self.mensaje} {self.dimension} {self.maximo}"
+
+
+
---
+++ foto.py
@@ -0,0 +1,34 @@
+from error import DimensionError
+
+
+
+class Foto():
+    MAX = 2500
+
+    def __init__(self, ancho: int, alto: int, ruta: str) -> None:
+        self.__ancho = ancho
+        self.__alto = alto
+        ruta = ruta
+
+    @property
+    def ancho(self) -> int:
+        return self.__ancho
+
+    @ancho.setter
+    def ancho(self, ancho) -> None:
+        if ancho < 1 or ancho > self.MAX:
+            raise DimensionError("Ancho fuera de rango", ancho, Foto.MAX)
+           
+              
+        self.__ancho = ancho
+
+    @property
+    def alto(self) -> int:
+        return self.__alto
+
+    @alto.setter
+    def alto(self, alto) -> None:
+        if alto < 1 or alto > self.MAX:
+            raise DimensionError("Alto fuera de rango", alto, Foto.MAX)
+        self.__alto = alto
+
